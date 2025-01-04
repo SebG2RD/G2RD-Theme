298aebadee431e543df9a83b547ab6b4175810af
SebG2RD <gerardsebastien@live.fr>
2025-01-04 13:02:09 +0100

    Ajout des variation/button/font

--- functions.php
+++ functions.php
@@ -9,5 +9,40 @@ function g2rd_register_assets() {
 
 }
 add_action('wp_enqueue_scripts', 'g2rd_register_assets');
-?>
+
+
+// function filter_theme_json_theme($theme_json) {
+// 	$new_data = [
+// 		"version" => 3,
+// 		"settings" => [
+// 			"color" => [
+// 				"palette" => [
+// 					[
+// 						"slug"  => "primary",
+// 						"color" => "#023047",
+// 						"name"  => "Nouvelle couleur principale",
+// 					],
+// 					[
+// 						"slug"  => "secondary",
+// 						"color" => "#FB8500",
+// 						"name"  => "Nouvelle couleur secondaire",
+// 					],
+//                 ],
+//             ],
+//         ],
+//         "styles" => [
+//             "elements" => [
+//                 "h1" => [
+//                     "typography" => [
+//                         "fontSize" => "var:preset|font-size|xl",
+//                     ],
+//                 ],
+//             ],
+//         ],	
+//     ];
+
+// 	return $theme_json->update_with( $new_data );
+// }
+
+// add_filter('wp_theme_json_data_theme', 'filter_theme_json_theme');
 
